dd21652ab0f763d13bc53a4c4e731c6d70ead489
faz-cxr <47987464+faz-cxr@users.noreply.github.com>
2023-03-17 17:40:16 +0400

    Update Conditions.py

--- pages/Conditions.py
+++ pages/Conditions.py
@@ -4,5 +4,9 @@ import pandas as pd
 df = pd.read_csv("conditions.csv")
 
 st.header("These are the conditions on which the model has been trained on")
-for condition in df["Topic"]:
-    st.write("- " + condition)
+with st.beta_container():
+    st.write(" ")
+    for i, condition in enumerate(df["Topic"], start=1):
+        st.write(f"{i}. {condition}", unsafe_allow_html=True)
+        st.write("<br>", unsafe_allow_html=True)
+    st.write(" ")
