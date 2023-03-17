181048bd88d38d05bf52141f520c80488934553f
faz-cxr <47987464+faz-cxr@users.noreply.github.com>
2023-03-17 18:03:29 +0400

    Update Conditions.py

--- pages/Conditions.py
+++ pages/Conditions.py
@@ -6,7 +6,16 @@ df = pd.read_csv("conditions.csv")
 st.header("These are the conditions on which the model has been trained on")
 with st.beta_container():
     st.write(" ")
-    search_term = st.text_input("Search for a topic:")
+    search_term = components.ace(
+        key="search",
+        placeholder="Search for a topic",
+        language="text",
+        height=50,
+        font_size=14,
+        show_gutter=False,
+        show_print_margin=False,
+        wrap=True,
+    )
     if search_term:
         filtered_df = df[df["Topic"].str.contains(search_term, case=False)]
     else:
