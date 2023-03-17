70d25760e0634dedfc3229f6cfebbc7d5e993248
faz-cxr <47987464+faz-cxr@users.noreply.github.com>
2023-03-17 17:57:23 +0400

    Update Conditions.py

--- pages/Conditions.py
+++ pages/Conditions.py
@@ -6,6 +6,12 @@ df = pd.read_csv("conditions.csv")
 st.header("These are the conditions on which the model has been trained on")
 with st.beta_container():
     st.write(" ")
-    for i, condition in enumerate(df["Topic"], start=1):
-        st.write(f"{i}. {condition}")
+    search_term = st.text_input("Search for a topic:")
+    if search_term:
+        filtered_df = df[df["Topic"].str.contains(search_term, case=False)]
+    else:
+        filtered_df = df
+    for i, row in filtered_df.iterrows():
+        topic = row["Topic"]
+        st.write(f"{topic}")
     st.write(" ")
