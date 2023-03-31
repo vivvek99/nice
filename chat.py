719a0228ac9669057d536995ac7537f5a3a8a8be
faz-cxr <faz.icxr@gmail.com>
2023-04-01 02:56:33 +0400

    New memory

--- chat.py
+++ chat.py
@@ -13,12 +13,10 @@ from index import load_vector_store
 import promptlayer
 promptlayer.api_key = "pl_2b1769e7202b6a141d4491fca41e308a"
 
-
 def clear_input() -> None:
     st.session_state["input"] = deepcopy(st.session_state["temp"])
     st.session_state["input_disabled"] = True
     st.session_state["temp"] = ""
-
 
 def get_input() -> str:
     st.text_area(
@@ -29,10 +27,9 @@ def get_input() -> str:
         label_visibility="hidden",
         key="temp",
         on_change=clear_input,
-        # disabled=st.session_state.input_disabled,
+        disabled=st.session_state.input_disabled,
     )
     return st.session_state["input"]
-
 
 def get_response(query: str) -> str:
     try:
@@ -43,37 +40,26 @@ def get_response(query: str) -> str:
         print(f"Get response error: {e}")
         return "Connection error. Please try again later."
 
-
+@st.cache_resource
 def get_chain():
-    if "chain" not in st.session_state:
-        st.session_state["chain"] = load_qa_with_sources_chain(
-            llm=PromptLayerChatOpenAI(
-                pl_tags=["chat-nice-st"],
-                temperature=0,
-                model_name=config.MODEL_NAME,
-                max_tokens=512,
-                request_timeout=int(180)
-            ),
-            chain_type="stuff",
-            memory=get_memory(),
-            prompt=get_prompt()
-        )
-    return st.session_state["chain"]
-
-def get_memory() -> ConversationSummaryBufferMemory:
-    if "memory" not in st.session_state:
-        st.session_state["memory"] = new_memory()
-    return st.session_state["memory"]
-
-
-def new_memory() -> ConversationSummaryBufferMemory:
-    llm = PromptLayerChatOpenAI(pl_tags=["chat-nice-st-memory"], model_name=config.MODEL_NAME, request_timeout=int(180))
-    memory = ConversationSummaryBufferMemory(
-        llm=llm,
-        memory_key="chat_history",
-        input_key="human_input"
+    chain = load_qa_with_sources_chain(
+        llm=PromptLayerChatOpenAI(
+            pl_tags=["chat-nice-st"],
+            temperature=0,
+            model_name=config.MODEL_NAME,
+            max_tokens=512,
+            request_timeout=int(180)
+        ),
+        chain_type="stuff",
+        memory=ConversationSummaryBufferMemory(
+            llm=PromptLayerChatOpenAI(pl_tags=["chat-nice-st-memory"], model_name=config.MODEL_NAME, request_timeout=int(180)),
+            max_token_limit=256,
+            memory_key="chat_history",
+            input_key="human_input"
+        ),
+        prompt=get_prompt()
     )
-    return memory
+    return chain
 
 def get_prompt() -> PromptTemplate:
     with open(config.PROMPT_PATH, "r") as f:
@@ -84,11 +70,15 @@ def get_prompt() -> PromptTemplate:
     )
     return prompt
 
-
 def get_documents(query: str) -> list[Document]:
     docsearch = get_docsearch()
-    return docsearch.similarity_search(query)
-
+    retriever = docsearch.as_retriever()
+    if "ai" in st.session_state:
+        last_resp = str(st.session_state["ai"][-1:])
+        fquery = last_resp+query
+    else:
+        fquery = query
+    return retriever.get_relevant_documents(fquery)
 
 def get_docsearch() -> Chroma:
     if "docsearch" not in st.session_state:
