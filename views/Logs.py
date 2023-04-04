049c2ace3d174394c8b5668424c58f9453a7dc2f
faz-cxr <faz.icxr@gmail.com>
2023-04-05 03:38:30 +0400

    Update

--- views/Logs.py
+++ views/Logs.py
@@ -3,27 +3,6 @@ import pandas as pd
 
 def createPage():
     st.markdown("### What others have been asking...")
-    html_temp = """
-                    <div style="background-color:{};padding:1px">
-                    
-                    </div>
-                    """
-    # with st.sidebar:
-    #     st.markdown("""
-    #     ### About 
-    #     \nAsk any medical question and get an explanation. You can even clear your MCQ doubts.
-    #     \nPlease note that a detailed question will get a detailed answer and will take 10-15 seconds.
-    #     \n\nDisclaimer: **DO NOT** use as a substitute for professional medical advice. This is meant for **EDUCATIONAL PURPOSES ONLY**.
-    #     \n### How does it work?
-    #     \nIt is trained on NICE CKS guidelines as in https://cks.nice.org.uk
-    #     \n*If the source is anything else other than NICE, then the answer could be wrong.*
-    #     """)
-    #     st.markdown("""[Source Code](https://github.com/faz-cxr/nice)
-    #     <a href = "mailto:fazeen.nasser@outlook.com?subject = Feedback&body = Message">Send Feedback</a><br>
-    #     <a href="https://www.buymeacoffee.com/fazeen" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png" alt="Buy Me A Coffee" style="height: 35px;width: 110px ;" ></a>
-    #     """,
-    #     unsafe_allow_html=True,
-    #     )
 
     # Read in the csv file
     df = pd.read_csv('./files/logs.csv')
